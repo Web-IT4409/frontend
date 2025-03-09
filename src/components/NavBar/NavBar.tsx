076149d661318f00fd11bf71dfb8f8029599b93c
Hao Vu <lesanmicxu000@gmail.com>
2025-03-09 21:08:29 +0700

    add bar

---
+++ src/components/NavBar/NavBar.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './NavBar.scss';
+
+const NavBar: React.FC = () => {
+    return (
+        <div className = "bar">
+            Top Bar
+            {/* <Icon />
+            <SearchBox />
+            <Messenger />
+            <Notifications />
+            <Options /> */}
+        </div>
+    );
+}
+
+
+export default NavBar; 
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import './Home.scss';
+import NavBar from '@/components/NavBar/NavBar'
 
 const Home: React.FC = () => {
     return (
         <div className="home">
             <h1>Welcome to the Home Page</h1>
+            <NavBar />
+            {/* <NewsFeed />
+            <Shortcuts />
+            <Contacts /> */}
+            
         </div>
     );
 };
